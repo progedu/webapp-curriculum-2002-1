94a6edc4753e20841a097578cdfb320ec526f2dd
Jun0422 <juv317life@yahoo.co.jp>
2021-06-13 00:11:01 +0900

    上級問題

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,1 +1,11 @@
-object Main {}
+object Main {
+  def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = {
+    var result: Boolean = true
+    for (i <- 0 until sortedSeq.length - 1) {
+      if (!ordered(sortedSeq(i), sortedSeq(i + 1))) {
+        result = false
+      }
+    }
+    result
+  }
+}
