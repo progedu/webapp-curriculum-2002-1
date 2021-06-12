71ed551d6e8254ae1363d601a06c32cb98fb6ee7
Jun0422 <juv317life@yahoo.co.jp>
2021-06-13 00:14:30 +0900

    上級問題_末尾再帰

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,13 @@
+import scala.annotation.tailrec
+
 object Main {
   def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = {
-    var result: Boolean = true
-    for (i <- 0 until sortedSeq.length - 1) {
-      if (!ordered(sortedSeq(i), sortedSeq(i + 1))) {
-        result = false
-      }
+    @tailrec
+    def isSortedRecursive(i: Int): Boolean = {
+      if (i == sortedSeq.length - 1) true
+      else if (!ordered(sortedSeq(i), sortedSeq(i + 1))) false
+      else isSortedRecursive(i + 1)
     }
-    result
+    if (sortedSeq.isEmpty) true else isSortedRecursive(0)
   }
 }
